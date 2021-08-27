6b489b5a2b19ab20fd3c368757bbc3abf5048fc3
Marlene <mquispel@est.emi.edu.bo>
2021-08-26 20:53:48 -0400

    feat[components]: Added new solver method linear algorithm

--- src/components/methods/linear-algorithm/LinearAlgorithm.js
+++ src/components/methods/linear-algorithm/LinearAlgorithm.js
@@ -3,31 +3,24 @@ import React, { useState } from 'react';
 import { ResultsTableLinearAlgorithm } from './ResultsTableLinearAlgorithm';
 
 import { useForm } from '../../../hooks/useForm';
-import { getMiddleProduct } from '../../../helpers/methods/middleProduct';
+import { getLinearAlgorithm } from '../../../helpers/methods/linearAlgorithm';
 
 export const LinearAlgorithm = () => {
 
     const [ solved, setSolved ] = useState( false );
     const [ table, setTable ] = useState({});
-    const [ equalDigits, setEqualDigits ] = useState( false );
 
-    const [ { semilla1, semilla2, cantidad }, handleInputChange ] = useForm({
-        semilla1: "",
-        semilla2: "",
+    const [ { semilla, cantidad, constanteK, numDigitos }, handleInputChange ] = useForm({
+        semilla: "",
         cantidad: "",
+        constanteK: "",
+        numDigitos: "",
     });
 
     const handleSubmit = ( event ) => {
         event.preventDefault();
         setSolved( true );
-        if ( semilla1.length >= 2 && semilla1.length === semilla2.length ) {
-            setTable( getMiddleProduct( parseInt( semilla1 ), parseInt( semilla2 ), parseInt( cantidad ) ) );
-            setEqualDigits( false );
-        } else {
-            setEqualDigits( true );
-            setSolved( false );
-        }
-
+        setTable( getLinearAlgorithm( parseInt( semilla ), parseInt( cantidad ), parseInt( constanteK ), parseInt( numDigitos ) ) );
     }
 
     const handleHideClick = ( event ) => {
@@ -43,21 +36,14 @@ export const LinearAlgorithm = () => {
             <hr />
             <div className="d-flex justify-content-center">
                 <div className="mt-5 jumbotron p-5 text-secondary d-inline-flex flex-column">
-                    <h3 className="font-weight-bold text-center display-5"> Algoritmo de los <br /> Productos medios </h3>
+                    <h3 className="font-weight-bold text-center display-5"> Algoritmo Congruencial <br /> Lineal </h3>
                     <hr />
                     <div className="">
-                        {
-                            equalDigits
-                            &&
-                            <div className="alert alert-danger" role="alert">
-                                Los numeros deben de ser <a className="alert-link"> de dos digitos o mayor ambos de la misma cantidad </a> de digitos.
-                            </div>
-                        }
                         <form onSubmit={ handleSubmit }>
                             <div className="form-group">
-                                <label> Semilla 1 </label>
+                                <label> Semilla x0 </label>
                                 <input
-                                    name="semilla1"
+                                    name="semilla"
                                     type="text"
                                     className="form-control"
                                     placeholder="6345"
@@ -67,28 +53,40 @@ export const LinearAlgorithm = () => {
                             </div>
 
                             <div className="form-group">
-                                <label> Semilla 2 </label>
-                                <input
-                                    name="semilla2"
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="6345"
-                                    onChange={ handleInputChange }
-                                />
-                                <small className="form-text text-muted"> Debe introducir un numero mayor a 9 </small>
-                            </div>
-
-
-                            <div className="form-group">
-                                <label> Cantidad </label>
+                                <label> Cantidad (Periodo) </label>
                                 <input
                                     name="cantidad"
                                     type="text"
                                     className="form-control"
-                                    placeholder="5"
+                                    placeholder="10"
+                                    onChange={ handleInputChange }
+                                />
+                                <small className="form-text text-muted"> Debe introducir un numero mayor a 9 </small>
+                            </div>
+
+
+                            <div className="form-group">
+                                <label> Constante K </label>
+                                <input
+                                    name="constanteK"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="543"
                                     onChange={ handleInputChange }
                                 />
                             </div>
+
+                            <div className="form-group">
+                                <label> Numero de digitos deseados </label>
+                                <input
+                                    name="numDigitos"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="3"
+                                    onChange={ handleInputChange }
+                                />
+                            </div>
+
                             <button type="submit" className="btn btn-success"> Generar </button>
                             {
                                 solved
